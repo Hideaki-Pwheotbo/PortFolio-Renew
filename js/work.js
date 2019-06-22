1f87a59c05f93b6a58517acab8dabb1500003582
Hideaki-Pwheotbo <o.hideaki0322@gmail.com>
2019-06-23 00:54:31 +0900

    20190622

---
+++ js/work.js
@@ -0,0 +1,144 @@
+Vue.component('cafearonde', {
+    template: `
+    <div class="work_Inner_Box">
+                        <div class="work_Inner_Box_Img">
+                            <a href="#" class="site_Img"><img src="images/cafe_1.jpg" alt="cafearonde"></a>
+                            <a href="#" class="link_Img"><img src="images/link.png" alt="link"><span>Cafe Aronde / WEB SITE</span></a>
+                            
+                        </div>
+                        <div class="work_Inner_Box_Text">
+                            <div class="work_Inner_Box_Text_Name">
+                                
+                                <h3><span>架空カフェサイト</span><br>Cafe Aronde</h3>
+                            </div>
+                            <div class="work_Inner_Box_Text_Item">
+                                <p>自身がコーヒー好きということもあって、カフェのサイトを作ってみたいと思い初めて作成した架空のサイト。</p><br>
+                                <p>何気なく通えて、ご近所さんの憩いの場でもあり、少しわかりずらい立地にある隠れ家のようなカフェをコンセプトに。</p><br>
+                                <br><p>また、季節の食材を使って丁寧につくる自家製のデザートを用意していると考えて、ここに通うすべての人が、ほっと一息つける場所を提供している。</p><br>
+                                
+                            </div>
+                        </div>
+                    </div>
+                    `
+})
+
+Vue.component('stickwall', {
+    template: `
+    <div class="work_Inner_Box">
+                        <div class="work_Inner_Box_Img">
+                            <a href="#" class="site_Img"><img src="images/stick_1.jpg" alt="stickwall"></a>
+                            <a href="#" class="link_Img"><img src="images/link.png" alt="link"><span>Stick Wall / WEB SITE</span></a>
+                            
+                        </div>
+                        <div class="work_Inner_Box_Text">
+                            <div class="work_Inner_Box_Text_Name">
+                                
+                                <h3><span>架空クライミングジムサイト</span><br>Stick Wall</h3>
+                            </div>
+                            <div class="work_Inner_Box_Text_Item">
+                                <p>自身がコーヒー好きということもあって、カフェのサイトを作ってみたいと思い初めて作成した架空のサイト。</p><br>
+                                <p>何気なく通えて、ご近所さんの憩いの場でもあり、少しわかりずらい立地にある隠れ家のようなカフェをコンセプトに。</p><br>
+                                <br><p>また、季節の食材を使って丁寧につくる自家製のデザートを用意していると考えて、ここに通うすべての人が、ほっと一息つける場所を提供している。</p><br>
+                                
+                            </div>
+                        </div>
+                    </div>`
+})
+
+Vue.component('secondone', {
+    template: `
+    <div class="work_Inner_Box">
+                        <div class="work_Inner_Box_Img">
+                            <a href="#" class="site_Img"><img src="images/second_1.jpg" alt="secondone"></a>
+                            <a href="#" class="link_Img"><img src="images/link.png" alt="link"><span>Second One / WEB SITE</span></a>
+                            
+                        </div>
+                        <div class="work_Inner_Box_Text">
+                            <div class="work_Inner_Box_Text_Name">
+                                
+                                <h3><span>架空通信建設会社サイト</span><br>Second One</h3>
+                            </div>
+                            <div class="work_Inner_Box_Text_Item">
+                                <p>自身がコーヒー好きということもあって、カフェのサイトを作ってみたいと思い初めて作成した架空のサイト。</p><br>
+                                <p>何気なく通えて、ご近所さんの憩いの場でもあり、少しわかりずらい立地にある隠れ家のようなカフェをコンセプトに。</p><br>
+                                <br><p>また、季節の食材を使って丁寧につくる自家製のデザートを用意していると考えて、ここに通うすべての人が、ほっと一息つける場所を提供している。</p><br>
+                                
+                            </div>
+                        </div>
+                    </div>`
+})
+
+Vue.component('weathermap', {
+    template: `
+    <div class="work_Inner_Box">
+                        <div class="work_Inner_Box_Img">
+                            <a href="#" class="site_Img"><img src="images/weather_1.jpg" alt="weathermap"></a>
+                            <a href="#" class="link_Img"><img src="images/link.png" alt="link"><span>Weather Map / WEB SITE</span></a>
+                            
+                        </div>
+                        <div class="work_Inner_Box_Text">
+                            <div class="work_Inner_Box_Text_Name">
+                                
+                                <h3><span>天気情報サイト</span><br>Weather Map</h3>
+                            </div>
+                            <div class="work_Inner_Box_Text_Item">
+                                <p>自身がコーヒー好きということもあって、カフェのサイトを作ってみたいと思い初めて作成した架空のサイト。</p><br>
+                                <p>何気なく通えて、ご近所さんの憩いの場でもあり、少しわかりずらい立地にある隠れ家のようなカフェをコンセプトに。</p><br>
+                                <br><p>また、季節の食材を使って丁寧につくる自家製のデザートを用意していると考えて、ここに通うすべての人が、ほっと一息つける場所を提供している。</p><br>
+                                
+                            </div>
+                        </div>
+                    </div>`
+})
+
+Vue.component('me', {
+    template: `
+    <div class="about_Inner_Box_Text_Item">
+        <p>小さいころから絵が好きで、専門学校にて3DCGを学ぶ。<br>その後電気工事、ネットワークや電話工事を経験。</p><br>
+        <p>ネットワークの仕事の中でサーバの設定を行い、通信試験の際に簡単なWEBサーバを立て活用したことがきっかけで、WEBに興味を持ち独学で始める。</p><br> 
+        <p>今まで携わっていたインフラの上で、自分で作ったものを見てみたい、また自分の作ったもので誰かに喜んでもらいたい。昔から好きだった絵やデザイン、ものを作る仕事に携わりたいと思い日々学習中。</p><br>
+        <br><p>ー　今の目標はエンジニアとして新しいものをつくること　ー</p><br>
+        <p>デザイン、JavaScript、PHPやその他の言語、またそのフレームワークなど、新しい技術を身につけることに加えて、今までのインフラの知識も活かしたいと思っています。</p><br>
+        <p>休日にはプログラミングの勉強や、暖かい時期はバイクに乗って趣味の写真撮影に出掛けます。</p>
+    </div>`
+})
+
+Vue.component('hobby', {
+    template: `
+    <div class="about_Inner_Box_Text_Item">
+        <p>四六時中パソコンをいじって、何か新しいことができないかといつも考えていますが、まだまだ技術が足りない。</p><br>
+        <p>写真撮影が好き。「どうやったらこんな写真が撮れるんだろう」そんなことをカメラを持ちながら考えています。</p><br> 
+        <p>車の運転や、バイクに乗ることが好き。マニュアル車の自分で操っている感覚がとても楽しい。なので、レースを見ることも好きで、初めてサーキットへ観戦に行ったときには大興奮しました。</p><br>
+        <br><p>バイクでは、トコトコゆっくり景色を見ながら走ること、その間にいい写真撮れそうな場所はないか探しています。</p><br>
+        
+    </div>`
+})
+
+Vue.component('wish', {
+    template: `
+    <div class="about_Inner_Box_Text_Item">
+        <p>四六時中パソコンをいじって、何か新しいことができないかといつも考えていますが、まだまだ技術が足りない。</p><br>
+        <p>写真撮影が好き。「どうやったらこんな写真が撮れるんだろう」そんなことをカメラを持ちながら考えています。</p><br> 
+        <p>車の運転や、バイクに乗ることが好き。マニュアル車の自分で操っている感覚がとても楽しい。なので、レースを見ることも好きで、初めてサーキットへ観戦に行ったときには大興奮しました。</p><br>
+        <br><p>バイクでは、スピードよりもゆっくり景色を見ながら走ること、その間にいい写真撮れそうな場所はないか探しています。</p><br>
+        
+    </div>`
+})
+
+
+const vm = new Vue({
+    el: '#app',
+    data: {
+        currentPage: 'cafearonde',
+        currentAbout: 'me'
+    },
+    methods: {
+        transPage: function(page) {
+            this.currentPage = page;
+        },
+
+        transAbout: function(about) {
+            this.currentAbout = about;
+        }
+    }
+})
